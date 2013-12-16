4997d8b6df2d4a70acc88a8a3b2592cf57be0033
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-12-16 23:18:37 +0100

    The application now reads a file

--- csv-reader.py
+++ csv-reader.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    reader = open('fichero.txt')
+    for row in reader:
+        print(row, end='')
+    return "Reading fichero.txt file"
 
 if __name__ == "__main__":
     app.run()
